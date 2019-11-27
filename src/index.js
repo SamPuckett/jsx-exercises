07d13edeb152f789c7cdb651fd11117bb583d87d
Samuel Ruiz <samuelpuckett77@gmail.com>
2019-11-27 13:11:51 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class MyThing extends React.Component {
+    render() {
+        return(
+            <div>MyThing</div>
+        );
+    }
+}
+
+ReactDOM.render(<MyThing/>, document.getElementById(root));
